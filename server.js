0e109da09c45db860f1f0d2cce79474ec8d70930
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-08-29 13:03:05 -0700

    modifying server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator');
 const cors = require('cors');
+const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT || 8080;
@@ -26,6 +27,9 @@ app.use(cors());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname , 'client', 'build', 'index.html'))
+    })
   }
 
 app.use('/', postRoutes);

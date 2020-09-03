198a907de67eb87c9263d78e51b9d65a7a8bd685
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-09-02 18:23:16 -0700

    conditionally render badge

--- client/src/core/Menu.js
+++ client/src/core/Menu.js
@@ -3,6 +3,16 @@ import {Link, withRouter} from 'react-router-dom'
 import {signout, isAuthenticated} from '../auth'
 import './menu.css'
 import Badge from './Badge';
+import { itemTotal } from '../post/cartFunctions';
+
+
+const isActive = (history, path) => {
+    if (history.location.pathname === path) {
+        return "";
+    } else {
+        return <Badge />;
+    }
+};
 
 
 const Menu = ({history}) => {
@@ -22,7 +32,7 @@ const Menu = ({history}) => {
                  <li className = "nav-item"><Link className="nav-link" to = {`/findpeople`}>Find People</Link></li>
                  <li className = "nav-item"><Link className="nav-link" to = {`/post/create`}>Create Listing</Link></li>
                  <li className = "nav-item"><Link className = "nav-link" to= "/">View All Listings</Link></li>
-                 <li className = "nav-item"><Link className = "nav-link" to= "/cart">View Cart<Badge /></Link></li>
+             <li className = "nav-item"><Link className = "nav-link" to= "/cart">View Cart{isActive(history, "/cart")}</Link></li>
                  <li className = "nav-item"><Link className = "nav-link" to= "/chat">Chat</Link></li>
                 <li className = "nav-item"><span
                  className="nav-link"

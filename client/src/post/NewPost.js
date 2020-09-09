8530ca72000ddffad4a90f4a15919d21d8bcc069
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-09-09 16:03:40 -0700

    editing new listing form

--- client/src/post/NewPost.js
+++ client/src/post/NewPost.js
@@ -79,7 +79,7 @@ class NewPost extends Component {
     newPostForm = (title, body, price, category, quantity) => (
         <form>
             <div className = "row">
-                <div className = "col-lg-2 d-flex justify-content-center">
+                <div className = "col-lg-5 d-flex justify-content-center">
                 <div className="form-group">
                 <label className="text-muted">Product Photo</label>
                 <input
@@ -91,7 +91,7 @@ class NewPost extends Component {
                 />
             </div>
              </div>
-             <div className = "col-lg-10">
+             <div className = "col-lg-7">
              <div className="form-group">
                 <label className="text-muted">Product Name</label>
                 <input
